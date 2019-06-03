556567ab4ed0f111ab61e86f6ebefec22a6a8f13
ttyn4519 <37261985+ttyn4519@users.noreply.github.com>
2019-06-03 16:16:04 +0900

    improve version 3.
    何個の数字が当たっているかを出すヒントを設定しました。

---
+++ kazuate_improve_3.py
@@ -0,0 +1,24 @@
+import random
+a1 = random.randint(0, 9)
+a2 =random.randint(0, 9)
+a3 = random.randint(0, 9)
+a4 = random.randint(0, 9)
+a = str(a1)+str(a2)+str(a3)+str(a4)
+hit=0
+print(a)
+b = input("4桁の数字を入力してください:")
+for i in range(4):
+    if b[i]>"9" or b[i]<"0":
+        print("数字を入力してください")
+    while len(b) != 4:
+        b = input("４桁の数字を入力してください:")
+    while a != b:
+        print("はずれです")
+        hit=0
+        for f in range(4):
+            if a[f]==b[f]:
+                hit=hit+1
+        print(str(hit)+"個あっています")
+        
+        b = (input("正しい答えを入力してください："))
+print("答えは"+str(a)+"でした。お見事！")
